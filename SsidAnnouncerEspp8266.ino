a410f6d1ee9c26fd2961ac0247d54eb120a96872
Erik Andrén Zachrisson <erik@zachrisson.info>
2015-05-30 22:19:35 +0200

    Remove unneeded code

--- SsidAnnouncerEspp8266.ino
+++ SsidAnnouncerEspp8266.ino
@@ -5,10 +5,7 @@ String ssids[3] = {"0900 Breakfast", "1200 Lunch", "1500 Afternoon tea"};
 Ticker ssidChanger;
 int ptr = 0;
 
-void changeSsid() {
-  int state = digitalRead(1);  // get the current state of GPIO1 pin
-  digitalWrite(1, !state);     // set pin to the opposite state
-  
+void changeSsid() {  
   WiFi.softAP(ssids[ptr].c_str());
   ptr++;
   if (ptr == 3) {
@@ -16,16 +13,8 @@ void changeSsid() {
   }
 }
 
-void setup() {
-  pinMode(1, OUTPUT);
-  digitalWrite(1, LOW);
-  
-  // put your setup code here, to run once:
-  //0.1 works ok
-  ssidChanger.attach(0.1, changeSsid);
+void setup() {  
+  ssidChanger.attach(0.5, changeSsid);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
